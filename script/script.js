894712ebd2fda209f83c7299c1ec0190d2b6b3bf
Jephtah Ogyefo Acquah <ogyefo.acquah@gmail.com>
2023-10-18 13:43:07 +0000

    Add the screen display  of the results and eventlisteners of for the numbers and the operators as well as activated the clear button

--- script/script.js
+++ script/script.js
@@ -1,23 +1,79 @@
-let firstValue;
+let firstsValue='';
 let operator;
-let secondValue;
-const add =function(a,b){
-    return a + b;
-};
-const subtract =function(a,b){
-    return a-b;
-};
-const multiply =function(a,b){
-    return a*b;
-};
-const divide =function(a,b){
-    return a/b;
-};
-const operate = function(firstValue,operator,secondValue){
-    return operator==="+"? add(firstValue,secondValue):
-        operator ==="-"? subtract(firstValue,secondValue):
-        operator ==='*'? multiply(firstValue,secondValue):
-        operator ==='/'? divide(firstValue,secondValue):"Error";
+let secondValue='';
+let displayStorage;
+const numbsButtons =document.querySelectorAll('.number');
+const equalButton = document.querySelector('.equal');
+const operatorButtons = document.querySelectorAll(".operator");
+const currentDisplayScreen =document.querySelector('.currentDisplayScreen');
+const previousDisplayScreen =document.querySelector('.previousDisplayScreen');
+const clearButton =document.querySelector('.clear');
+clearButton.addEventListener('click',()=>{
+    currentDisplayScreen.textContent ='';
+    previousDisplayScreen.textContent ='';
+    firstsValue='';
+    secondValue='';
+});
+
+const inputValue =function(values){
+    console.log(values);
+    firstsValue+=values;
+    console.log(firstsValue);
+    currentDisplayScreen.textContent =firstsValue;
 }
 
-console.log(operate(2,'/',3));
+
+numbsButtons.forEach(num =>
+    num.addEventListener('click', ()=>inputValue(num.textContent))
+    
+
+  );
+
+function handlers(op){
+    operator =op;
+    secondValue = firstsValue;
+    firstsValue=''
+    previousDisplayScreen.textContent = secondValue + operator;
+    currentDisplayScreen.textContent ='';
+}
+
+function calculate(){
+
+   secondValue =Number(secondValue);
+   firstsValue = Number(firstsValue);
+    
+    if (operator==="+"){
+        secondValue+= firstsValue;
+       console.log(secondValue);
+        
+    }
+    else if (operator ==="-"){
+        secondValue -= firstsValue;
+        console.log(secondValue)
+    }else if (operator ==="x"){
+        
+        secondValue *= firstsValue;
+        console.log(secondValue)
+    }else if (operator ==="/"){
+        secondValue/= firstsValue;
+        if (secondValue===Infinity){secondValue="Error"};
+    }
+    currentDisplayScreen.textContent=secondValue;
+    previousDisplayScreen.textContent ='';
+
+}
+equalButton.addEventListener('click',calculate)
+
+
+operatorButtons.forEach(operator =>
+    operator.addEventListener('click',()=>{
+        return handlers(operator.textContent);
+    }))
+
+
+
+
+
+
+
+
